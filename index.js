414ab6befbcaad8b7ad0819d0d80ffd52bac750d
Jaouadi Hadir <hadirjaouedi@gmail.com>
2026-08-21 03:18:32 +0200

    Mise à jour : nouvelle version avec MongoDB

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express =require('express')
 const app= express()
-const port=3000
+const mongoose = require('mongoose');
+const port=3001
 app.get("/",(req,res)=> {
     res.sendFile("./views/homepage.html",{root: __dirname})  ;
 })
-app.listen(port,()=>{
+mongoose
+.connect("mongodb://hadirjaouedi_db_user:MYWCwmroG13aNPCT@ac-omrjkwr-shard-00-00.vkky72a.mongodb.netall-data:27017,ac-omrjkwr-shard-00-01.vkky72a.mongodb.net:27017,ac-omrjkwr-shard-00-02.vkky72a.mongodb.net:27017/?ssl=true&replicaSet=atlas-rgxh02-shard-0&authSource=admin&appName=Cluster0")
+.then(()=> {
+    app.listen(port,()=>{
     console.log(`http://localhost:${port}/`)
 })
+})
+.catch((err)=> {console.log(err)})
+;

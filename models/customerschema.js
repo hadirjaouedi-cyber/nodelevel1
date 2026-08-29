6c347ebbe0448b030eb19ab527b9900f99c075f6
Jaouadi Hadir <hadirjaouedi@gmail.com>
2026-08-29 09:24:01 +0200

    lesson9

---
+++ models/customerschema.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema=mongoose.Schema;
+const userSchema=new Schema({
+    firstName:String,
+    lastName:String,
+    email:String,
+    phomeNumber:String,
+    age:Number,
+    country:String,
+    gender:String,
+
+});
+const Mydata=mongoose.model("customer",userSchema);
+module.exports=Mydata
